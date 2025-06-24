f8012272beb034baf882520d54605c5c6516eafd
juhyun-97 <rlawngus1316@naver.com>
2025-06-24 12:54:16 +0900

    20250624 eclipse

--- 06_java/08_Polymorphism/src/com/kh/array/practice2/Application.java
+++ 06_java/08_Polymorphism/src/com/kh/array/practice2/Application.java
@@ -3,99 +3,21 @@ package com.kh.array.practice2;
 import java.util.Arrays;
 import java.util.Scanner;
 
-import javax.swing.text.View;
-
-import com.kh.array.practice1.model.Student;
 import com.kh.array.practice2.controller.MemberController;
 import com.kh.array.practice2.model.Member;
-import com.kh.array.practice2.view.MemberView;
+
 
 public class Application {
+	
+	Scanner sc = new Scanner(System.in);
+	MemberController mc = new MemberController();
+	
 
 	public static void main(String[] args) {
 		
-		Scanner sc = new Scanner(System.in);
-		Member[] members = new Member[3];
-		int count = 0;
+		Application app = new Application();
+		app.mainMenu();
 
-		
-		while(true) {
-			System.out.println("최대 등록 가능한 회원 수는 3명입니다.");
-			System.out.println("현재 등록된 회원 수는 " + count + "명입니다.");
-			System.out.println("1. 새 회원 등록");
-			System.out.println("2. 회원 정보 수정");
-			System.out.println("3. 전체 회원 정보 출력");
-			System.out.println("9. 끝내기");
-			System.out.print("메뉴 번호 : ");
-			int num = Integer.parseInt(sc.nextLine());
-			switch(num) {
-			case 1: 
-				Member m = new Member();
-				System.out.print("아이디 : ");
-				m.setId(sc.nextLine());
-				System.out.print("이름 : ");
-				m.setName(sc.nextLine());
-				System.out.print("비밀번호 : ");
-				m.setPwd(sc.nextLine());
-				System.out.print("이메일 : ");
-				m.setEmail(sc.nextLine());
-				System.out.print("성별(M/F) : ");
-				m.setGender(sc.nextLine().charAt(0));
-				System.out.print("나이 : ");
-				m.setAge(Integer.parseInt(sc.nextLine()));
-				
-				members[count++] = m;
-				break;
-				
-//				 * 		 -> 아이디를 입력 받았는데 기존 멤버 배열에 아이디가 있는 경우
-//				 * 			"중복된 아이디입니다. 다시 입력해주세요." 출력 후
-//				 * 			다시 아이디 입력부터 나올 수 있게 처리
-			case 2:
-				System.out.print("수정할 회원의 아이디 : ");
-				String id = sc.nextLine();
-				for(int i = 0; i < members.length; i++) {
-				if(id.equals(members[i].getId())) {
-					System.out.println("중복된 아이디입니다. 다시 입력해주세요.");
-				} else {
-					return;
-				}
-				}
-				System.out.print("수정할 이름 : ");
-				String name = sc.nextLine();
-				System.out.print("수정할 이메일 : ");
-				String email = sc.nextLine();
-				System.out.print("수정할 비밀번호 : ");
-				String pwd = sc.nextLine();
-				
-				for(int i = 0; i < members.length; i++) {
-					if(members[i] != null && members[i].getId().equals(id)) {
-						members[i].setName(name);
-						members[i].setEmail(email);
-						members[i].setPwd(pwd);
-					}
-				}
-				
-				break;
-			case 3:
-				for(Member member : members) {
-					if(member != null) {
-						System.out.println(member);
-					}
-				}
-				break;
-			case 9:
-				System.out.println("끝내기!");
-				break;
-			default :
-				System.out.println("잘못 입력하셨습니다. 다시 입력해주세요");
-				break;
-			}
-		}
-		
-		
-		
-		
-		
 		
 		/*
 		 * 회원 수가 3명이 최대 등록 가능
@@ -132,5 +54,107 @@ public class Application {
 		 * */
 		
 	}
-}
-
+	
+	// 전체 메뉴
+	public void mainMenu() {
+		boolean check = true;
+		while(check) {
+			System.out.println("최대 등록 가능한 회원 수는 3명입니다.");
+			System.out.println("현재 등록된 회원 수는 " + mc.count + "명입니다.");
+			if(mc.count < 3) {
+				System.out.println("1. 새 회원 등록");
+			} else {
+				System.out.println("회원 수가 모두 꽉 찼기 때문에 일부 메뉴만 오픈합니다.");
+			}
+			System.out.println("2. 회원 정보 수정");
+			System.out.println("3. 전체 회원 정보 출력");
+			System.out.println("9. 끝내기");
+			System.out.print("메뉴 번호 : ");
+			int num = Integer.parseInt(sc.nextLine());
+			switch(num) {
+			case 1: 
+				insertMember();
+				break;
+			case 2:
+				updateMember();
+				break;
+			case 3:
+				printAll();
+				break;
+			case 9:
+				System.out.println("프로그램 종료");
+				check = false;
+				break;
+			default :
+				System.out.println("잘못 입력하셨습니다. 다시 입력해주세요");
+			}
+		}
+	}
+	
+	// 메뉴 번호 1 : insertMember
+	public void insertMember() {
+//		 * 		 -> 아이디를 입력 받았는데 기존 멤버 배열에 아이디가 있는 경우
+//		 * 			"중복된 아이디입니다. 다시 입력해주세요." 출력 후
+//		 * 			다시 아이디 입력부터 나올 수 있게 처리
+		Member m = new Member();
+		System.out.print("아이디 : ");
+		m.setId(sc.nextLine());
+		
+		// 아이디 체크 해야하는 부분!
+		boolean idCheck = mc.idCheck(m.getId()) == -1; // 아이디가 없는 경우!
+		// 아이디 이외에 내용이 아이디가 하나라도 있는 경우 안나오게 처리
+		if(idCheck) {
+			System.out.print("이름 : ");
+			m.setName(sc.nextLine());
+			System.out.print("비밀번호 : ");
+			m.setPwd(sc.nextLine());
+			System.out.print("이메일 : ");
+			m.setEmail(sc.nextLine());
+			System.out.print("성별(M/F) : ");
+			m.setGender(sc.nextLine().charAt(0));
+			System.out.print("나이 : ");
+			m.setAge(Integer.parseInt(sc.nextLine()));
+			
+			// 추가 로직 와야하는 부분
+			mc.addInfo(m);
+		}		
+	}
+	// 메뉴 번호 2 : updateMember
+	public void updateMember() {
+		System.out.print("수정할 회원의 아이디 : ");
+		String id = sc.nextLine();
+		
+		// 아이디 체크 해야하는 부분!
+		boolean idUpdate = mc.idCheck(id) == -1;
+		if(idUpdate) {
+			System.out.println("회원 정보가 없습니다.");
+		} else {
+			System.out.print("수정할 이름 : ");
+			String name = sc.nextLine();
+			System.out.print("수정할 이메일 : ");
+			String email = sc.nextLine();
+			System.out.print("수정할 비밀번호 : ");
+			String pwd = sc.nextLine();
+			
+			// 수정하는 로직!
+			mc.updateInfo(id, name, email, pwd);
+		}
+	}
+	// 메뉴 번호 3 전체 회원 정보 출력
+	public void printAll() {
+		for(Member member : mc.getMembers()) {
+			if(member != null) {
+				System.out.println(member);
+			}
+	  }
+	
+   }
+	
+	// 메서드 분리!
+	// 아이디 체크! <- 등록할 때도 필요! 수정할 때도 필요! 여기저기 필요한 경우!
+	
+		
+	}
+	
+	
+	
--- 06_java/08_Polymorphism/src/com/kh/array/practice2/controller/MemberController.java
+++ 06_java/08_Polymorphism/src/com/kh/array/practice2/controller/MemberController.java
@@ -1,63 +1,39 @@
 package com.kh.array.practice2.controller;
-import java.util.Arrays;
-import java.util.Scanner;
 
 import com.kh.array.practice2.model.Member;
+
 public class MemberController {
-    
-    // 공간만 만들거냐
-    public Member[] members = new Member[3]; 
-    
-    // 객체부터 생성할거냐
-    //Member[] members = {new Member(), new Member(), new Member()}; 
-    
-    // 멤버수
-    //public int countMember() {
-    //	return members.length;
-    //}
-    
-    // 아이디 체크 -> 배열을 전부 확인하고 아이디가 있는지 없는지 여부
-    public void idCheck() {
-        for(Member member : members) {
-            //System.out.println(member.getId());
-            System.out.println(member != null && member.getId()!=null);
-        }
-    
-   
+
+	private Member[] members = new Member[3];
+	public int count = 0;
 	
-	System.out.println(Arrays.toString(members));
-	
-	for(int i = 0; i > members.length; i++) {
-		System.out.println(members[i]);
+	// members - getter
+	public Member[] getMembers() {
+		return members;
 	}
 	
-//	System.out.print("아이디 : ");
-//	String id = sc.nextLine();
-//	System.out.print("이름 : ");
-//	String name = sc.nextLine();
-//	System.out.print("비밀번호 : ");
-//	String pwd = sc.nextLine();
-//	System.out.print("이메일 : ");
-//	String email = sc.nextLine();
-//	System.out.print("성별(M/F) : ");
-//	String gender = sc.nextLine();
-//	System.out.print("나이 : ");
-//	int age = sc.nextInt();
+	// 추가 로직!
+	public void addInfo(Member m) {
+		members[count++] = m;
+	}
 
-//	private Member m = new Member();
-//
-//	public String saveData(Member memberData) {
-//		m.setId(memberData.getId());
-//		m.setName(memberData.getName());
-//		m.setPwd(memberData.getPwd());
-//		m.setEmail(memberData.getEmail());
-//		m.setGender(memberData.getGender());
-//		m.setAge(memberData.getAge());
-//		return "중복된 아이디입니다. 다시 입력해주세요";
-//	}
-//	
-//	public Member confirmData() {
-//		return m;
-//	}
-}
+	public int idCheck(String id) {
+
+		for (int i = 0; i < members.length; i++) {
+			if (members[i] != null && members[i].getId().equals(id)) {
+				// 기존 배열에서 아이디가 있는 경우
+				return i;
+			}
+		}
+
+		// 아이디가 없는 경우!
+		return -1;
+	}
+
+	public void updateInfo(String id, String name, String email, String pwd) {
+		int index = idCheck(id);
+		members[index].setName(name);
+		members[index].setEmail(email);
+		members[index].setPwd(pwd);
+	}
 }
