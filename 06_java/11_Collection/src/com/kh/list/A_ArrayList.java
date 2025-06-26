1b723958813afb2a1459f9d11795d22c86304879
juhyun-97 <rlawngus1316@naver.com>
2025-06-26 17:51:50 +0900

    20250626 eclipse

--- 06_java/11_Collection/src/com/kh/list/A_ArrayList.java
+++ 06_java/11_Collection/src/com/kh/list/A_ArrayList.java
@@ -1,6 +1,8 @@
 package com.kh.list;
 
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 
 import com.kh.list.model.Person;
 
@@ -74,7 +76,57 @@ public class A_ArrayList {
 		
 		// 5. get : 해당 인덱스의 객체 가져오기
 	    System.out.println(list.get(0));
+	    
+	    // 6. subList : 기존 리스트에서 원하는 만큼 추출해서 새로운 리스트 반환
+	    list.add(new Person("전현무", "삼성동", 47));
+	    list.add(new Person("기안84", "과천시", 40));
+	    
+	    List<Person> subList = list.subList(0, 2); // 어디배열 어디배열까지 자를거냐(배열 0부터 배열 2 앞에까지~)
+	    System.out.println(subList);
+	    
+	    // 7. addAll : 컬렉션을 통째로 추가
+	    list.addAll(subList); // (sublist) 처리한 리스트를 뒤에 추가로 배열
+	    
+	    // 8. isEmpty : 컬렉션이 비어있는지
+	    System.out.println("리스트가 비어있는지? " + list.isEmpty()); // 값이 있기때문에 비어있지않다
+	    
+	    // 리스트에 저장된 사람들의 평균 연령 출력
+	    int sum = 0;
+	    for(int i = 0; i < list.size(); i++) {
+	    	Person person = list.get(i);
+	    	sum += person.getAge();
+	    }
+	    System.out.println(sum / list.size());
+	    
+	    
+	    // 저장된 사람들의 이름만 출력
+	    for(Person person : list) {
+	    	System.out.println(person.getName());
+	    }
+	    
+	    // 나이 순서대로 정렬 -> 이름순으로
+	    Collections.sort(list);
+	    System.out.println(list);
+	    
+	    
+	    // 9. clear : 싹 비우기
+	    list.clear();
+	    
+		System.out.println(list);
+	}
+	
+	public void method3() {
+		List<String> list = new ArrayList<>();
+		list.add("banana");
+		list.add("orange");
+		list.add("apple");
+		list.add("mango");
+		list.add("grape");
 		
+		// 10. 오름차순 정렬 : Comparable 인텊이스를 구현하고 있는 요소를 가지고 비교값들 반환하여 정렬
+		Collections.sort(list);
+		// 11. 내림차순 정렬 : 정확하게 내림차순 정렬하려면 먼저 오름차순(sort) 처리 후 내림차순(reverse) 정렬 
+		Collections.reverse(list);
 		System.out.println(list);
 	}
 	
@@ -82,6 +134,7 @@ public class A_ArrayList {
 		A_ArrayList a = new A_ArrayList();
 		//a.method1();
 		a.method2();
+		//a.method3();
 		
 	}
 
---
+++ 06_java/11_Collection/src/com/kh/practice2/model/Member.java
@@ -0,0 +1,10 @@
+package com.kh.practice2.model;
+
+import java.util.ArrayList;
+
+public class Member {
+	private String name;
+	private int age;
+	private int coupon;
+	private ArrayList<Book> bookList = new ArrayList<>();
+}
